7c445e264f46a3fb872d2a6921945fd4f825774c
rohsameep <94984035+rohsameep@users.noreply.github.com>
2023-09-19 11:45:48 +0530

    Add files via upload

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "488941cabe81683492f5f776067b2e9f";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
